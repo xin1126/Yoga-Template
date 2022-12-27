0fbb5dfcd871bfdfc0d1b68b036ddab24119a2f1
xin1126 <z26319491z@gmail.com>
2022-12-27 15:08:52 +0800

    fix: tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,24 @@ module.exports = {
     './nuxt.config.{js,ts}'
   ],
   theme: {
-    extend: {}
+    extend: {
+      // 自訂顏色
+      colors: {
+        primary: '#907A7B',
+        secondary: '#C0A4A5',
+        light: '#F5F0E7',
+        dark: '#707070'
+      }
+    },
+    container: {
+      center: true,
+      screens: {
+        '2xl': '1280px'
+      },
+      padding: {
+        DEFAULT: '12px'
+      }
+    }
   },
   plugins: []
 }
