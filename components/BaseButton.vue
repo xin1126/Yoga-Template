ac2b3561d78bdb0318addfea07c90045ee1cf0c6
xin1126 <z26319491z@gmail.com>
2022-12-27 15:09:52 +0800

    feat: BaseButton components

---
+++ components/BaseButton.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+  props: {
+    rounded: {
+      type: String,
+      default: ''
+    },
+    padding: {
+      type: String,
+      required: true
+    },
+    border: {
+      type: Boolean,
+      default: false
+    },
+    bg: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<template>
+  <button
+    type="button"
+    :class="[
+      rounded === 'xl' ? 'rounded-xl' : 'rounded',
+      border ? 'border border-primary !text-primary hover:bg-secondary hover:!text-white' : 'hover:opacity-80',
+      bg === 'bg-light' ? 'text-dark' : 'text-white',
+      bg,
+      padding
+    ]"
+    class="duration-200 font-bold"
+  >
+    <slot />
+  </button>
+</template>
