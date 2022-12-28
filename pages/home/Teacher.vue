969a33ea50828fe53b8e9a7e793eca79369a7769
xin1126 <z26319491z@gmail.com>
2022-12-28 18:22:05 +0800

    feat: Home Teacher

---
+++ pages/home/Teacher.vue
@@ -0,0 +1,38 @@
+
+<template>
+  <div class="bg-light">
+    <div class="container sm:py-[64px] pb-12 pt-8 xl:flex">
+      <img class="mr-[30px] h-[630px] min-w-[540px] rounded-xl xl:block hidden" src="@/assets/images/yoga-4.jpeg" alt="yoga">
+      <div class="xl:mt-[30px]">
+        <h2 class="text-primary text-3xl mb-2 xl:text-start text-center">頂尖師資、專業教學</h2>
+        <div class="flex justify-center">
+          <p class="text-secondary text-xl mb-6">強力師資，經歷豐富，協會認證。運用引導式教<br class="hidden sm:block xl:hidden"/>學，針對不同需求的人制定專屬課程。</p>
+        </div>
+        <ul class="text-dark xl:items-start flex flex-col items-end">
+          <li class="mb-4 bg-white sm:px-12 sm:py-10 p-4 flex xl:w-[120%] xl:relative xl:left-[-90px] rounded-xl items-center md:w-[90%] w-full">
+            <img class="sm:w-[94px] sm:h-[94px] w-[48px] h-[48px] mr-5" src="@/assets/images/teacher-1.png" alt="teacher">
+            <div>
+              <p class="text-xl mb-2">Carol Tang 老師</p>
+              <p class="mb-2">教學經歷 10 年</p>
+              <p>瑜珈讓我重新認識自己的身體，也讓我有了不一樣的人生。</p>
+            </div>
+          </li>
+          <li class="mb-4 bg-white sm:px-8 sm:py-6 p-4 flex rounded-xl items-center md:w-[80%] xl:w-full w-full">
+            <img class="sm:w-[68px] sm:h-[68px] w-[48px] h-[48px] mr-5" src="@/assets/images/teacher-2.png" alt="teacher">
+            <div>
+              <p class="text-xl mb-2">Kanae 老師</p>
+              <p>教學經歷 10 年</p>
+            </div>
+          </li>
+          <li class="bg-white sm:px-8 sm:py-6 p-4 flex rounded-xl items-center md:w-[80%] xl:w-full w-full">
+            <img class="sm:w-[68px] sm:h-[68px] w-[48px] h-[48px] mr-5" src="@/assets/images/teacher-3.png" alt="teacher">
+            <div>
+              <p class="text-xl mb-2">Asa Ifrit 老師</p>
+              <p>教學經歷 10 年</p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,14 @@
 <script>
 import Banner from './home/Banner.vue'
 import Effect from './home/Effect.vue'
+import Teacher from './home/Teacher.vue'
 
 export default {
   name: 'IndexPage',
   components: {
     Banner,
     Effect,
+    Teacher
   },
   layout: 'BaseLayout'
 }
@@ -16,5 +18,6 @@ export default {
   <div>
     <Banner class="md:mb-10 mb-8" />
     <Effect class="md:mb-16 mb-12" />
+    <Teacher class="md:mb-16 mb-12" />
   </div>
 </template>
