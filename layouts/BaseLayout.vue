1527c49b68f338b1479890856e92caed94b08f60
xin1126 <z26319491z@gmail.com>
2023-01-05 18:04:09 +0800

    feat: AsideCommunity

--- layouts/BaseLayout.vue
+++ layouts/BaseLayout.vue
@@ -3,17 +3,20 @@
     <Navbar />
     <Nuxt class="pt-[86px]" />
     <Downbar />
+    <AsideCommunity class="hidden lg:block" />
   </div>
 </template>
 
 <script>
 import Navbar from "./Navbar.vue";
 import Downbar from "./Downbar.vue";
+import AsideCommunity from "./AsideCommunity.vue";
 
 export default {
   components: {
     Navbar,
     Downbar,
+    AsideCommunity,
   },
 };
 </script>
