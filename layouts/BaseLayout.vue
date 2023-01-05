c1e1d7f7691880bdd8447b1b9addc8928d89cfee
xin1126 <z26319491z@gmail.com>
2023-01-05 17:50:48 +0800

    feat: DownBar

--- layouts/BaseLayout.vue
+++ layouts/BaseLayout.vue
@@ -2,15 +2,18 @@
   <div>
     <Navbar />
     <Nuxt class="pt-[86px]" />
+    <Downbar />
   </div>
 </template>
 
 <script>
 import Navbar from "./Navbar.vue";
+import Downbar from "./Downbar.vue";
 
 export default {
   components: {
     Navbar,
+    Downbar,
   },
 };
 </script>
