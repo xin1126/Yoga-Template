6d3bb0f706a1ba038ace49d88cc08ea52a25d9d3
xin1126 <z26319491z@gmail.com>
2023-01-06 18:13:45 +0800

    fix: CourseIntroduction Search RWD

--- pages/courseIntroduction/components/Search.vue
+++ pages/courseIntroduction/components/Search.vue
@@ -1,21 +1,25 @@
 <template>
   <div class="mb-12 bg-secondary xl:mb-14">
-    <div class="container flex py-5">
-      <div class="mr-2 flex">
-        <input
-          type="text"
-          placeholder="請輸入要搜尋的課程"
-          class="rounded-l py-2 pl-4 placeholder:text-primary"
-        />
+    <div class="container flex flex-col-reverse py-5 sm:flex-row">
+      <div class="flex sm:mr-2">
+        <div class="flex-auto">
+          <input
+            type="text"
+            placeholder="請輸入要搜尋的課程"
+            class="w-full rounded-l py-2 pl-4 placeholder:text-primary"
+          />
+        </div>
         <button
-          class="rounded-r bg-primary px-4 py-2 text-white duration-500 hover:bg-secondary/80"
+          class="whitespace-nowrap rounded-r bg-primary px-4 py-2 text-white duration-500 hover:bg-primary/80"
         >
           搜尋
         </button>
       </div>
-      <select class="rounded px-3 text-primary">
-        <option>全部課程</option>
-      </select>
+      <div>
+        <select class="mb-2 rounded px-3 py-2 text-primary sm:mb-0">
+          <option>全部課程</option>
+        </select>
+      </div>
     </div>
   </div>
 </template>
